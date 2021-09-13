f53b50fdca073c72f6708bea614280b5cf047c3b
DESKTOP-RPE3AMR\nomuk <s203388q@st.go.tuat.ac.jp>
2021-09-13 22:17:41 +0900

    shiftjisするとエラーが起きるので英語で試す.

--- timeTable.py
+++ timeTable.py
@@ -1,14 +1,14 @@
 class timeTable:
     dic = {
-        "Œ—j“ú":"Monday", 
-        "‰Î—j“ú":"Tuesday",
-        "…—j“ú":"Wednesday",
-        "–Ø—j“ú":"Thursday",
-        "‹à—j“ú":"Friday"
+        "a":"Monday", 
+        "b":"Tuesday",
+        "c":"Wednesday",
+        "d":"Thursday",
+        "e":"Friday"
     }
 
     def getResponse(self, message):
         for _dic in self.dic:
             if _dic == message:
                 return self.dic[message]
-        return "•s–¾‚ÈƒŠƒeƒ‰ƒ‹‚Å‚·. "
+        return "unknown literal "
